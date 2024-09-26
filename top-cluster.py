701c56567e95504936322d180907a390bcc4c4c0
Corey Lowman <clowman@lambdal.com>
2024-09-26 09:47:29 -0400

    Adding nprocs to top cluster ouput

--- top-cluster.py
+++ top-cluster.py
@@ -44,12 +44,13 @@ while True:
         host: dict(util=0, power_usage=0, memory_usage=0, num_gpus=0, num_procs=0)
         for host in hosts
     }
-    cluster_stats = dict(util=0, power_usage=0, memory_usage=0, num_gpus=0)
+    cluster_stats = dict(util=0, power_usage=0, memory_usage=0, num_gpus=0, num_procs=0)
     for host, output in zip(hosts, outputs):
         gpu_stats[host] = {}
         for gpu, stats in enumerate(output.splitlines()):
             if "," not in stats:
                 node_stats[host]["num_procs"] += 1
+                cluster_stats["num_procs"] += 1
                 continue
 
             util, power_draw, power_limit, memory_used, memory_total = map(
@@ -78,16 +79,14 @@ while True:
         node_stats[host]["memory_usage"] /= node_stats[host]["num_gpus"]
         node_stats[host]["power_usage"] /= node_stats[host]["num_gpus"]
 
-    max_host_length = max(map(len, hosts))
-
     print(f"==={datetime.datetime.now()}")
-    print(f"{'name':>10}\t{'util':>10}\t{'power':>10}\t{'memory':>10}")
+    print(f"{'name':>10}\t{'util':>10}\t{'power':>10}\t{'memory':>10}\t{'nprocs':>10}")
     print(
-        f"{'cluster':>10}\t{cluster_stats['util']:>9.1f}%\t{cluster_stats['power_usage']:>9.1f}%\t{cluster_stats['memory_usage']:>9.1f}%"
+        f"{'cluster':>10}\t{cluster_stats['util']:>9.1f}%\t{cluster_stats['power_usage']:>9.1f}%\t{cluster_stats['memory_usage']:>9.1f}%\t{cluster_stats['num_procs']:>10}"
     )
     for host, stats in node_stats.items():
         print(
-            f"{host:>10}\t{stats['util']:>9.1f}%\t{stats['power_usage']:>9.1f}%\t{stats['memory_usage']:>9.1f}%"
+            f"{host:>10}\t{stats['util']:>9.1f}%\t{stats['power_usage']:>9.1f}%\t{stats['memory_usage']:>9.1f}%\t{stats['num_procs']:>10}"
         )
     print("===")
 
