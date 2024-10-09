c6073a5a542a824562f05b613f51f3fb38eabb41
Corey Lowman <clowman@lambdal.com>
2024-10-09 14:28:46 +0000

    Adding sketch of 2d parallelism chapter

---
+++ 07-2d-parallelism/README.md
@@ -0,0 +1,125 @@
+# Training a 405B model with 2d Parallelism
+
+So far we've just been using data parallel techniques. You may have heard of other parallelism techniques, and indeed the [Llama 405B paper](https://ai.meta.com/research/publications/the-llama-3-herd-of-models/) actually uses 4D parallelism when training the 405B model:
+
+1. Data parallel (FSDP as we've learned)
+2. Tensor parallel (this chapter)
+3. Context parallel (For long context lengths)
+4. Pipeline/model parallel
+
+In this chapter we are going to be adding tensor parallelism to our 405b training script from [chapter 6](../06-training-llama-405b/) to see what improvements we get.
+
+## Basics: What is tensor parallelism?
+
+Some operations can be easily split among multiple workers without having to share any parameters. If you remember with fully sharded data parallel, we still have to gather all the weights onto each node. That is due to the way the operations we were wrapping worked.
+
+Let's think about applying a ReLU Activation to a tensor at first - well we could easily split the input tensor across our workers and all the workers could separately apply the ReLU function to each of their parts.
+
+Tensor Parallelism is most often applied with Linear layers, which occur a lot in Transformer architecture. Basically the weight & bias matrix in the Linear layer are split up across your workers, and then when applying the forward to the tensor, only the tensor needs to be split across the workers, not the weights.
+
+This ends up being much cheaper/faster!
+
+## Pytorch Reference Docs
+
+For completeness here are the relevant docs/guides from pytorch on how to achieve this:
+- [API docs](https://pytorch.org/docs/stable/distributed.tensor.parallel.html#tensor-parallelism-torch-distributed-tensor-parallel)
+- [2d Parallelism Tutorial](https://pytorch.org/tutorials/intermediate/TP_tutorial.html#large-scale-transformer-model-training-with-tensor-parallel-tp)
+- [Device Mesh tutorial](https://pytorch.org/tutorials/recipes/distributed_device_mesh.html)
+
+## Implementation
+
+There are a couple of parts to implementing this. For starters we are going to need what pytorch calls a [Device Mesh](https://pytorch.org/docs/stable/distributed.html#torch.distributed.device_mesh.DeviceMesh). Why? Because this will let us split workers into groups based on what node they are on.
+
+For a high level view first, we are going to:
+
+1. Internal to each node do tensor parallelism (so split Linear layers up across the node)
+2. Across nodes do data parallelism (what we've been doing with FSDP so far)
+
+A DeviceMesh helps us work with this.
+
+```python
+# NOTE: assumes all nodes have the same number of gpus
+num_local_gpus = torch.cuda.device_count()
+num_nodes = dist.get_world_size() // num_local_gpus
+mesh = dist.device_mesh.init_device_mesh("cuda", (num_nodes, num_local_gpus), ("dp", "tp"))
+```
+
+### Model Architecture Reference
+
+For reference for the next couple of sections, here is the architecture for 405B:
+
+```python
+LlamaForCausalLM(
+  (model): LlamaModel(
+    (embed_tokens): Embedding(128256, 16384)
+    (layers): ModuleList(
+      (0-125): 126 x LlamaDecoderLayer(
+        (self_attn): LlamaSdpaAttention(
+          (q_proj): Linear(in_features=16384, out_features=16384, bias=False)
+          (k_proj): Linear(in_features=16384, out_features=1024, bias=False)
+          (v_proj): Linear(in_features=16384, out_features=1024, bias=False)
+          (o_proj): Linear(in_features=16384, out_features=16384, bias=False)
+          (rotary_emb): LlamaRotaryEmbedding()
+        )
+        (mlp): LlamaMLP(
+          (gate_proj): Linear(in_features=16384, out_features=53248, bias=False)
+          (up_proj): Linear(in_features=16384, out_features=53248, bias=False)
+          (down_proj): Linear(in_features=53248, out_features=16384, bias=False)
+          (act_fn): SiLU()
+        )
+        (input_layernorm): LlamaRMSNorm((16384,), eps=1e-05)
+        (post_attention_layernorm): LlamaRMSNorm((16384,), eps=1e-05)
+      )
+    )
+    (norm): LlamaRMSNorm((16384,), eps=1e-05)
+    (rotary_emb): LlamaRotaryEmbedding()
+  )
+  (lm_head): Linear(in_features=16384, out_features=128256, bias=False)
+)
+```
+
+TODO we apply FSDP to the decoder layer block, but need to apply TP to the linear layers/MLP?
+
+### Applying tensor parallelism to our model
+
+Note that this is done *before* passing our model to FSDP. Luckily this all works very seamlessly with meta models.
+
+```python
+from torch.distributed.tensor.parallel import ColwiseParallel, RowwiseParallel, parallelize_module
+```
+TODO
+
+
+### Using our mesh with FSDP
+
+For this, we can just pass our dp mesh directly to the FSDP constructor:
+
+```python
+model = FSDP(..., device_mesh=mesh["dp"])
+```
+
+Note that this allows us to try other versions of sharding.
+
+You can also just pass the entire mesh into device_mesh. TODO what should be done??
+
+Here are three options:
+1. `sharding_strategy=ShardingStrategy.FULL_SHARD` and `device_mesh=mesh["dp"]`
+
+TODO
+
+2. `sharding_strategy=ShardingStrategy.SHARD_GRAD_OP` and `device_mesh=mesh`
+
+TODO
+
+3. `sharding_strategy=ShardingStrategy.HYBRID_SHARD` and `device_mesh=mesh`
+
+TODO
+
+4. `sharding_strategy=ShardingStrategy._HYBRID_SHARD_ZERO2` and `device_mesh=mesh`
+
+TODO
+
+
+## Questions
+
+Does tp.SequenceParallel apply the sharding for you?
---
+++ 07-2d-parallelism/train_llm.py
@@ -0,0 +1,471 @@
+import argparse
+from contextlib import contextmanager
+import functools
+from itertools import chain
+import json
+import multiprocessing
+import os
+import time
+from pathlib import Path
+import logging
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+from torch import distributed as dist
+import torch.distributed.tensor.parallel as tp
+from torch.distributed._tensor import Shard, Replicate
+from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
+    apply_activation_checkpointing,
+    checkpoint_wrapper,
+)
+from torch.distributed.elastic.multiprocessing.errors import record
+from torch.distributed.fsdp.fully_sharded_data_parallel import (
+    FullyShardedDataParallel,
+    CPUOffload,
+    ShardingStrategy,
+)
+from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
+from torch.distributed.checkpoint.state_dict import (
+    get_state_dict,
+    set_state_dict,
+    StateDictOptions,
+)
+from torch.distributed.checkpoint.state_dict_loader import load
+from torch.distributed.checkpoint.state_dict_saver import save
+
+
+import wandb
+import tqdm
+import datasets
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    default_data_collator,
+)
+from transformers.models.llama.modeling_llama import (
+    LlamaDecoderLayer,
+    LlamaFlashAttention2,
+    LlamaForCausalLM,
+)
+
+LOGGER = logging.getLogger(__name__)
+
+
+@record
+def main():
+    parser = _get_parser()
+    args = parser.parse_args()
+
+    dist.init_process_group()
+
+    num_local_gpus = torch.cuda.device_count()
+
+    rank = dist.get_rank()
+    local_rank = rank % num_local_gpus
+    world_size = dist.get_world_size()
+
+    assert (
+        world_size % num_local_gpus == 0
+    ), "This script assumes all nodes have the same amount of GPUs"
+    num_nodes = world_size // num_local_gpus
+
+    mesh = dist.device_mesh.init_device_mesh(
+        "cuda",
+        (num_nodes, num_local_gpus),
+        mesh_dim_names=("dp", "tp"),
+    )
+
+    logging.basicConfig(
+        format=f"[rank={rank}] [%(asctime)s] %(levelname)s:%(message)s",
+        level=logging.INFO,
+    )
+
+    LOGGER.info(os.environ)
+    LOGGER.info(args)
+    LOGGER.info(f"local_rank={local_rank} rank={rank} world size={world_size}")
+
+    device = torch.device(f"cuda:{local_rank}")
+    dtype = torch.bfloat16
+    torch.cuda.set_device(device)
+
+    torch.manual_seed(args.seed)
+
+    LOGGER.info(f"Loading model from HF_HOME={os.environ['HF_HOME']}")
+
+    model: LlamaForCausalLM
+    config = AutoConfig.from_pretrained(args.model_name, use_cache=False)
+    if rank == 0:
+        with torch.device("cpu"):
+            model = AutoModelForCausalLM.from_pretrained(
+                args.model_name,
+                torch_dtype=dtype,
+                attn_implementation="flash_attention_2",
+                use_cache=False,
+            )
+    else:
+        with torch.device("meta"):
+            model = AutoModelForCausalLM.from_config(
+                config,
+                torch_dtype=dtype,
+                attn_implementation="flash_attention_2",
+            )
+    LOGGER.info(f"{sum(p.numel() for p in model.parameters())} model parameters")
+
+    LOGGER.info(f"Before Tensor Parallel: {_get_cuda_mem_stats()}")
+
+    tp.parallelize_module(
+        model,
+        mesh["tp"],
+        {
+            "model.embed_tokens": tp.RowwiseParallel(),
+            # NOTE: not sure if we can do sequence parllel on this one?
+            "model.norm": tp.SequenceParallel(),
+            "lm_head": tp.ColwiseParallel(
+                input_layouts=Shard(1), output_layouts=Replicate()
+            ),
+        },
+    )
+    for layer in model.model.layers:
+        tp.parallelize_module(
+            layer,
+            mesh["tp"],
+            {
+                "input_layernorm": tp.SequenceParallel(),
+                # The input to self_attn (which is the output from the SequenceParallel input_layer_norm) will be sharded on dimension 1, but we wanted it to be the whole tensor.
+                # self_attn is what we are FSDP'ing down below. we don't apply TP here.
+                "self_attn": tp.PrepareModuleInput(
+                    input_layouts=Shard(dim=1), desired_input_layouts=Replicate()
+                ),
+                "post_attention_layernorm": tp.SequenceParallel(),
+                "mlp": tp.PrepareModuleInput(
+                    input_layouts=Shard(dim=1), desired_input_layouts=Replicate()
+                ),
+                "mlp.gate_proj": tp.ColwiseParallel(),
+                "mlp.up_proj": tp.ColwiseParallel(),
+                "mlp.down_proj": tp.RowwiseParallel(),
+            },
+        )
+
+    LOGGER.info(f"Before FSDP: {_get_cuda_mem_stats()}")
+
+    model = FullyShardedDataParallel(
+        model,
+        device_id=local_rank,
+        param_init_fn=lambda m: m.to_empty(device=device, recurse=False),
+        sync_module_states=True,
+        auto_wrap_policy=functools.partial(
+            transformer_auto_wrap_policy,
+            transformer_layer_cls={LlamaFlashAttention2},
+        ),
+        sharding_strategy=ShardingStrategy.FULL_SHARD,
+        cpu_offload=CPUOffload(offload_params=args.cpu_offload == "on"),
+        device_mesh=mesh["dp"],
+        use_orig_params=True,
+    )
+
+    LOGGER.info(f"After FSDP: {_get_cuda_mem_stats()}")
+    LOGGER.info(f"FSDP architecture: {model}")
+
+    # Applying gradient checkpointing - note that only the LlamaDecoderLayer supports this,
+    # so we can just reuse our existing wrap_policy.
+    apply_activation_checkpointing(
+        model,
+        checkpoint_wrapper_fn=checkpoint_wrapper,
+        auto_wrap_policy=functools.partial(
+            transformer_auto_wrap_policy,
+            # TODO include embedding?
+            transformer_layer_cls={LlamaDecoderLayer},
+        ),
+    )
+
+    # NOTE: since this can download data, make sure to do the main process first on each node
+    # since we manually specified HF_HOME to be a node local drive.
+    with rank_ordered(should_go_first=local_rank == 0):
+        train_data = _load_and_preprocess_data(args, config)
+    LOGGER.info(f"{len(train_data)} training samples")
+
+    dataloader = DataLoader(
+        train_data,
+        batch_size=args.batch_size,
+        collate_fn=default_data_collator,
+        num_workers=1,
+        prefetch_factor=2,
+        # NOTE: this sampler will split dataset evenly across workers
+        sampler=DistributedSampler(train_data, shuffle=True, drop_last=True),
+    )
+    LOGGER.info(f"{len(dataloader)} batches per epoch")
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, fused=True)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, T_max=1000, eta_min=args.lr * 1e-2
+    )
+
+    exp_dir: Path = Path(args.save_dir) / args.experiment_name
+
+    # NOTE: full_state_dict=False means we will be saving sharded checkpoints.
+    ckpt_opts = StateDictOptions(full_state_dict=False, cpu_offload=True)
+
+    # attempt resume
+    state = {
+        "epoch": 0,
+        "global_step": 0,
+        "epoch_step": 0,
+        "running_loss": 0,
+    }
+    resumed = False
+    if (exp_dir / "state.json").exists():
+        sharded_model_state, sharded_optimizer_state = get_state_dict(
+            model, optimizer, options=ckpt_opts
+        )
+        load(
+            dict(model=sharded_model_state, optimizer=sharded_optimizer_state),
+            checkpoint_id=exp_dir / "checkpoint",
+        )
+        set_state_dict(
+            model,
+            optimizer,
+            model_state_dict=sharded_model_state,
+            optim_state_dict=sharded_optimizer_state,
+            options=ckpt_opts,
+        )
+        lr_scheduler.load_state_dict(
+            torch.load(
+                exp_dir / "lr_scheduler.pt", map_location=device, weights_only=True
+            )
+        )
+        with open(exp_dir / "state.json") as fp:
+            state = json.load(fp)
+        resumed = True
+    LOGGER.info(f"Resumed={resumed} | {state}")
+    dist.barrier()
+
+    if (exp_dir.is_mount() and rank == 0) or (
+        not exp_dir.is_mount() and local_rank == 0
+    ):
+        LOGGER.info(f"Creating experiment root directory")
+        exp_dir.mkdir(parents=True, exist_ok=True)
+    dist.barrier()
+
+    if rank == 0:
+        wandb.init(
+            project="distributed-training-guide",
+            dir=exp_dir,
+            name=args.experiment_name,
+            id=args.experiment_name,
+            resume="must" if resumed else None,
+            save_code=True,
+            config={
+                "args": vars(args),
+                "training_data_size": len(train_data),
+                "num_batches": len(dataloader),
+                "world_size": world_size,
+            },
+        )
+
+    timers = {k: LocalTimer(device) for k in ["data", "forward", "backward", "update"]}
+
+    for state["epoch"] in range(state["epoch"], args.num_epochs):
+        LOGGER.info(f"Begin epoch {state['epoch']} at step {state['epoch_step']}")
+
+        progress_bar = tqdm.tqdm(range(len(dataloader)), disable=True)
+        if state["epoch_step"] > 0:
+            progress_bar.update(state["epoch_step"])
+
+        batches = iter(dataloader)
+
+        for i_step in range(len(dataloader)):
+            with timers["data"], torch.no_grad():
+                batch = next(batches)
+                batch = {k: v.to(device=device) for k, v in batch.items()}
+
+            if i_step < state["epoch_step"]:
+                # NOTE: for resuming
+                continue
+
+            with timers["forward"], tp.loss_parallel():
+                outputs = model(**batch)
+
+            with timers["backward"], tp.loss_parallel():
+                outputs.loss.backward()
+
+            with timers["update"]:
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad(set_to_none=args.cpu_offload == "off")
+
+            state["global_step"] += 1
+            state["epoch_step"] += 1
+            state["running_loss"] += outputs.loss.item()
+            progress_bar.update(1)
+
+            if state["global_step"] % args.log_freq == 0:
+                info = {
+                    "global_step": state["global_step"],
+                    "lr": lr_scheduler.get_last_lr()[0],
+                    "running_loss": state["running_loss"] / args.log_freq,
+                    "epoch": state["epoch"],
+                    "epoch_progress": state["epoch_step"] / len(dataloader),
+                    "num_batches_remaining": len(dataloader) - i_step,
+                    **_get_cuda_mem_stats(),
+                    "time/total": sum(t.avg_elapsed_ms() for t in timers.values()),
+                    **{
+                        f"time/{k}": timer.avg_elapsed_ms()
+                        for k, timer in timers.items()
+                    },
+                }
+
+                LOGGER.info(info)
+                if rank == 0:
+                    wandb.log(info, step=state["global_step"])
+
+                torch.cuda.reset_peak_memory_stats(device)
+                state["running_loss"] = 0
+                for t in timers.values():
+                    t.reset()
+
+            if state["global_step"] % args.ckpt_freq == 0:
+                LOGGER.info("Saving checkpoint.")
+                dist.barrier()
+                # NOTE: we have to call this on ALL ranks
+                sharded_model_state, sharded_optimizer_state = get_state_dict(
+                    model, optimizer, options=ckpt_opts
+                )
+                save(
+                    dict(model=sharded_model_state, optimizer=sharded_optimizer_state),
+                    checkpoint_id=exp_dir / "checkpoint",
+                )
+                if rank == 0:
+                    torch.save(lr_scheduler.state_dict(), exp_dir / "lr_scheduler.pt")
+                    with open(exp_dir / "state.json", "w") as fp:
+                        json.dump(state, fp)
+                dist.barrier()
+
+        state["epoch_step"] = 0
+
+
+def _get_cuda_mem_stats():
+    mem = torch.cuda.memory_stats(torch.cuda.current_device)
+    return {
+        "curr_alloc_in_gb": 1e-9 * mem["allocated_bytes.all.current"],
+        "peak_alloc_in_gb": 1e-9 * mem["allocated_bytes.all.peak"],
+        "curr_resv_in_gb": 1e-9 * mem["reserved_bytes.all.current"],
+        "peak_resv_in_gb": 1e-9 * mem["reserved_bytes.all.peak"],
+    }
+
+
+def _load_and_preprocess_data(args, config):
+    """
+    Function created using code found in
+    https://github.com/huggingface/transformers/blob/v4.45.1/examples/pytorch/language-modeling/run_clm_no_trainer.py
+    """
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+
+    data = datasets.load_dataset(args.dataset_name, trust_remote_code=True)
+
+    column_names = data["train"].column_names
+    text_column_name = "text" if "text" in column_names else column_names[0]
+
+    def tokenize_function(examples):
+        return tokenizer(examples[text_column_name])
+
+    tokenized_datasets = data.map(
+        tokenize_function,
+        batched=True,
+        remove_columns=column_names,
+        num_proc=multiprocessing.cpu_count(),
+        load_from_cache_file=True,
+        desc="Running tokenizer on dataset",
+    )
+
+    seq_length = args.seq_length or tokenizer.model_max_length
+    if seq_length > config.max_position_embeddings:
+        seq_length = min(1024, config.max_position_embeddings)
+
+    # Main data processing function that will concatenate all texts from our dataset and generate chunks of block_size.
+    def group_texts(examples):
+        # Concatenate all texts.
+        concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+        total_length = len(concatenated_examples[list(examples.keys())[0]])
+        # We drop the small remainder, and if the total_length < block_size  we exclude this batch and return an empty dict.
+        # We could add padding if the model supported it instead of this drop, you can customize this part to your needs.
+        if total_length > seq_length:
+            total_length = (total_length // seq_length) * seq_length
+        # Split by chunks of max_len.
+        result = {
+            k: [t[i : i + seq_length] for i in range(0, total_length, seq_length)]
+            for k, t in concatenated_examples.items()
+        }
+        result["labels"] = result["input_ids"].copy()
+        return result
+
+    lm_datasets = tokenized_datasets.map(
+        group_texts,
+        batched=True,
+        num_proc=multiprocessing.cpu_count(),
+        load_from_cache_file=True,
+        desc=f"Grouping texts in chunks of {seq_length}",
+    )
+
+    return lm_datasets["train"]
+
+
+@contextmanager
+def rank_ordered(*, should_go_first: bool):
+    if should_go_first:
+        yield
+    dist.barrier()
+    if not should_go_first:
+        yield
+    dist.barrier()
+
+
+class LocalTimer:
+    def __init__(self, device: torch.device):
+        if device.type == "cpu":
+            self.synchronize = lambda: torch.cpu.synchronize(device=device)
+        elif device.type == "cuda":
+            self.synchronize = lambda: torch.cuda.synchronize(device=device)
+        self.measurements = []
+        self.start_time = None
+
+    def __enter__(self):
+        self.synchronize()
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if traceback is None:
+            self.synchronize()
+            end_time = time.time()
+            self.measurements.append(end_time - self.start_time)
+        self.start_time = None
+
+    def avg_elapsed_ms(self):
+        return 1000 * (sum(self.measurements) / len(self.measurements))
+
+    def reset(self):
+        self.measurements = []
+        self.start_time = None
+
+
+def _get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--experiment-name", default=None, required=True)
+    parser.add_argument("--dataset-name", default=None, required=True)
+    parser.add_argument("--model-name", default=None, required=True)
+    parser.add_argument("--save-dir", default="../outputs")
+    parser.add_argument("--seed", default=0, type=int)
+    parser.add_argument("--num-epochs", default=100, type=int)
+    parser.add_argument("--lr", default=3e-5, type=float)
+    parser.add_argument("--batch-size", default=1, type=int)
+    parser.add_argument("--log-freq", default=100, type=int)
+    parser.add_argument("--ckpt-freq", default=500, type=int)
+    parser.add_argument("--cpu-offload", default="on", choices=["on", "off"])
+    parser.add_argument("--seq-length", default=None, type=int)
+    return parser
+
+
+if __name__ == "__main__":
+    main()
